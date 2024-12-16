149b0954db494c3d2a312550269a287293a067a5
ni9eball <peterkayode2018@gmail.com>
2024-12-16 18:46:38 +0100

    "updated name"

--- src/app/page.js
+++ src/app/page.js
@@ -58,7 +58,7 @@ export default async function Home() {
           place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+          Calabasas Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
